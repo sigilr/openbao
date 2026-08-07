9389949c26566bd3352b8cc5388390fe988c3c50
Tamal Saha <tamal@appscode.com>
2026-08-07 18:34:12 +0600

    docs: add website documentation for the hazelcast database plugin
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- website/sidebars.ts
+++ website/sidebars.ts
@@ -344,6 +344,7 @@ const sidebars: SidebarsConfig = {
                         "secrets/databases/index",
                         "secrets/databases/cassandra",
                         "secrets/databases/custom",
+                        "secrets/databases/hazelcast",
                         "secrets/databases/influxdb",
                         "secrets/databases/mysql-maria",
                         "secrets/databases/postgresql",
