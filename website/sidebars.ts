04bebd78c374b670975bea9358e0ceddbe6994fb
Tamal Saha <tamal@appscode.com>
2026-08-07 17:49:34 +0600

    docs: add website documentation for the elasticsearch database plugin
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- website/sidebars.ts
+++ website/sidebars.ts
@@ -344,6 +344,7 @@ const sidebars: SidebarsConfig = {
                         "secrets/databases/index",
                         "secrets/databases/cassandra",
                         "secrets/databases/custom",
+                        "secrets/databases/elasticsearch",
                         "secrets/databases/influxdb",
                         "secrets/databases/mysql-maria",
                         "secrets/databases/postgresql",
